aa5d1914070500d9102baff03e2c7f12c18ff632
Stuart Harland <essjayhch@users.noreply.github.com>
2020-06-25 15:31:42 +0100

    Fix sc

--- sc.tf
+++ sc.tf
@@ -1,13 +1,13 @@
 resource kubernetes_storage_class nfs {
   metadata {
-    name      = local.name
+    name = local.name
   }
 
   storage_provisioner = "kubernetes.io/gce-pd"
   reclaim_policy      = "Retain"
 
   parameters = {
-    type = "pd-standard"
-    replication_type = "regional-pd"
+    type             = "pd-standard"
+    replication-type = "regional-pd"
   }
 }
